48216ba00a2308b6a52902efa7cff14b238429ed
neetharmaliackal <neethamaliackal@gmail.com>
2022-06-06 07:20:46 +0530

    config file modified

--- index.js
+++ index.js
@@ -3,24 +3,11 @@ const app = express();              //Instantiate an express app, the main work 
 const port = 5000;                  //Save the port number where your server will be listening
 const router=express.Router();
 //Idiomatic expression in express to route and respond to a client request
-app.get('/api/home', (req, res) => {        //get requests to the root ("/") will route here
+app.get('/api/home/personalsurvey/form', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('survey_personal.json', {root: __dirname});      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
-// var personalSurvey=require('personal/api');
-// router.use("personal/api",personalSurvey);
-// app.use("personal/api",personalSurvey);
-// app.get('*', function(req, res) {        
-// res.sendFile('survey_personal.json', {root: __dirname});     
- 
-// });
-// var personalSurvey=require('personal/api')
-// app.get('/', (req, res) => { 
 
-// });
-//helmet
-// app.use(nocache());
-// app.use("personal/api");
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
