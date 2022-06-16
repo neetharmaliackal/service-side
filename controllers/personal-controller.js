bb4c02e6f619f4aa7b6a31979006c75fa46f72a2
neetharmaliackal <neethamaliackal@gmail.com>
2022-06-16 13:16:03 +0530

    id and pending status added

--- controllers/personal-controller.js
+++ controllers/personal-controller.js
@@ -1,75 +1,32 @@
 const fs = require('fs/promises');
 
 
-async function writeDataToFile(data) {
- 
+async function writeDataToFile(data,userId) {
+    data.status="pending";
+     data.id=userId;
     let fileContents = await fs.readFile('persondata.json', { encoding: 'utf8' });
     fileContents = JSON.parse(fileContents);
     console.log("filecontent",fileContents);
     fileContents.push(data);
+    // fileContents.push(userId);
     await fs.writeFile('persondata.json', JSON.stringify(fileContents, null, 2), { encoding: 'utf8' });
 
-//  const file= fs.writeFile('persondata.json', new Buffer(JSON.stringify(data)), 
-//  function (err) {
-//   if (err) return console.log(err);
-//     console.log('Hello World ');
-//    }
-//    );
-// fs.appendFile('persondata.json', (JSON.stringify(data)), function (err) {
-//   if (err) throw err;
-//   // console.log('Saved!');
-// });
-// const feed=fs.readFile('persondata.json', (err, data) => {
-//   console.log("file read",data);
-// })
-
-// var userdata = [];
-//  userdata.push(feed);
-// console.log(feed);
 }
-  // console.log("data", data);
-  //  fs.writefileSync('persondata.json',data);
-  //----------------
-  // user = [data];
-  // if (user.length > 0) {
-  //   // user = [];
-  //   if(user.firstName!=data.firstName && user.lastName!=data.lastName && user.middleName!=data.middleName)
-  //   { 
-  //     user = [...user, data];
-  //   }
+
+// async function writeApprovedUsersToFile(data,userId) {
+ 
+//   let fileContents = await fs.readFile('ApprovedPersons.json', { encoding: 'utf8' });
+//   fileContents = JSON.parse(fileContents);
+//   console.log("filecontent",fileContents);
+//   fileContents.push(data);
+//   fileContents.push(userId);
+//   await fs.writeFile('ApprovedPersons.json', JSON.stringify(fileContents, null, 2), { encoding: 'utf8' });
+
+// }
   
-  // } else {
-  //   user = data;
-  // }
-  // console.log("user", user);
-  //  -------------
-  // user = fs.readFile('persondata.json', new Buffer(JSON.stringify(data)), 
-  // function (err) {
-  //  if (err) return console.log(err);
-  //    console.log('Hello World ');
-  //   }
-  //   );
-    // user = [...user, data];
-    //  fs.writeFile('persondata.json', new Buffer(JSON.stringify(user)), 
-    // function (err) {
-    //  if (err) return console.log(err);
-    //    console.log('Hello World ');
-    //   }
-      // );
-      // console.log("user", user);
-      //----------
-  //  if('persondata.json' !=null){
-  //    Datasaved=true;
-  //    fs.appendFile('persondata.json',new Buffer(JSON.stringify(data)), function (err) {
-  //     if (err) throw err;
-  //     console.log('Saved!');
-  //   });
-  //   // fs.writefileSync('persondata.json', new Buffer(JSON.stringify(data)));
-  //   // console.log('Saved!');
-  // fs.appendFileSync("persondata.json",new Buffer(JSON.stringify(data)));
-  //  }
 
 
 module.exports = {
   writeDataToFile,
+  // writeApprovedUsersToFile
 };
--- index.js
+++ index.js
@@ -9,18 +9,47 @@ const router=express.Router();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const {v4 : uuidv4} = require('uuid')
 
 app.get('/api/home/personalsurvey/form', (req, res) => {       
     res.sendFile('survey_personal.json', {root: __dirname});     
                                                      
 });
 app.post('/api/home/personalsurvey/formsubmission', (req, res) => { 
-    controller.writeDataToFile(req.body);
+    const userId = uuidv4();
+    controller.writeDataToFile(req.body, userId );
+    console.log(req.body, userId);
     res.send("success");
 });
 app.get('/api/home/personalsurvey/userdata', (req, res) => {  
     // res.json({status: 'success'})   ;  
     res.sendFile('persondata.json', {root: __dirname});     
+                                                     
+});
+
+app.put('/api/home/personalsurvey/approved', (req, res) => { 
+    // const {firstName, middleName,lastName,radio,address} = req.body 
+    // const userId = uuidv4();
+    // console.log(userId,req.body);
+    // controller.writeApprovedUsersToFile(req.body, userId );
+    // repo.create({
+    //     userId,
+    //     firstName, 
+    //     middleName,
+    //     lastName,
+    //     radio,
+    //     address
+    //   })
+    //   res.send('Information submitted!');
+    //   res.sendFile('ApprovedPersons.json', {root: __dirname}); 
+    //   res.send(ApprovedPersons.json);
+  
+     
+                                                     
+});
+app.put('/api/home/personalsurvey/rejected', (req, res) => {  
+    // res.json({status: 'success'})   ;  
+     
                                                      
 });
 
